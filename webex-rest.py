37afe289437f48525a40783a4b806e4036474392
oscsalga <oscsalga@cisco.com>
2020-07-03 13:29:20 -0500

    Buscando conversaciones por nombre

--- webex-rest.py
+++ webex-rest.py
@@ -10,6 +10,22 @@ token = "Bearer NmJjZGVjZmMtZWEyMC00NThmLWJjNGMtMGNkZTU4N2Y1NWU1MGFhYWYxYzgtZWM5
 headers = {"Accept": "application/json",
            "Authorization": token}
 
-r = requests.get(url, headers=headers)
+r = requests.get(url, headers=headers).json()
 
-print(json.dumps(r.json(), indent=2))
+# print(r.status_code) # IMPRIME 200 SI ESTÁ BIEN
+#p rint(r.ok) # IMPRIME True SI ESTÁ BIEN
+
+for x in range(0, len(r["items"])):
+    if "Francisco" in r["items"][x]["title"]:
+        id = r["items"][x]["id"]
+        print("https://webexapis.com/v1/messages?roomId=" + id)
+        mensaje = requests.get("https://webexapis.com/v1/messages?roomId=" + id, headers=headers).json()
+
+        for y in range(len(mensaje["items"]) * -1, 1):
+            try:
+                print(f"Mensaje Numero: {y}")
+                print(mensaje["items"][y]["text"])
+                print("*" * 100)
+            except:
+                pass
+

f2541f08a9f1d215e910df49f1531b98bf8f2d3e
oscsalga <oscsalga@cisco.com>
2020-07-03 13:41:50 -0500

    Hidding token

--- webex-rest.py
+++ webex-rest.py
@@ -6,7 +6,8 @@ import requests
 import json
 
 url = "https://webexapis.com/v1/rooms"
-token = "Bearer NmJjZGVjZmMtZWEyMC00NThmLWJjNGMtMGNkZTU4N2Y1NWU1MGFhYWYxYzgtZWM5_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f"
+clave = ""
+token = f"Bearer {clave}"
 headers = {"Accept": "application/json",
            "Authorization": token}
 

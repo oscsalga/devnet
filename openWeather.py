f4455e969d35f1912faae877dcb175fb0008a873
oscsalga <oscsalga@cisco.com>
2020-07-03 13:45:45 -0500

    Hidding Token

--- openWeather.py
+++ openWeather.py
@@ -2,7 +2,8 @@ import requests
 import json
 import time
 
-urlOpen = "http://api.openweathermap.org/data/2.5/weather?id=3529947&units=metric&APPID=60dc4a5a9cbaec2de2279f548a35b806"
+clave = ""
+urlOpen = f"http://api.openweathermap.org/data/2.5/weather?id=3529947&units=metric&APPID={clave}"
 
 while True:
     respuesta = requests.get(urlOpen).json()

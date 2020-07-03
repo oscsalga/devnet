0d0b9e5bcece0781a028da57fb8cc2678024416c
oscsalga <oscsalga@cisco.com>
2020-07-02 20:50:10 -0500

    fixed open weather

--- openWeather.py
+++ openWeather.py
@@ -4,13 +4,11 @@ import time
 
 urlOpen = "http://api.openweathermap.org/data/2.5/weather?id=3529947&units=metric&APPID=60dc4a5a9cbaec2de2279f548a35b806"
 
-
-
-
 while True:
     respuesta = requests.get(urlOpen).json()
 
     name = json.dumps(respuesta, indent=2)
-    print(respuesta["main"]["temp"])
+    print("Temperatura real: " + str(respuesta["main"]["temp"]))
+    print("Feels like:: " + str(respuesta["main"]["feels_like"]) + "\n")
     time.sleep(2)
 

92f36ea38346b61a565c83b6d6a8e7ee5ce75fd4
oscar <oscar@oscar>
2020-07-06 21:52:42 -0500

    Added some commentary on restconf

--- restconf.py
+++ restconf.py
@@ -11,11 +11,20 @@ urlBase = "https://ios-xe-mgmt-latest.cisco.com"
 port = 9443
 url = f"{urlBase}:{port}/restconf/data/native"
 
+
+## HEADER PUEDE QUEDAR COMO AUTHORIZATION BASIC Y USER Y PASS COMO BASE64
 headers = {"Accept": "application/yang-data+json",
-           "Content-Type": "application/yang-data+json"}
+           "Content-Type": "application/yang-data+json",
+           "Authorization": "Basic ZGV2ZWxvcGVyOkMxc2NvMTIzNDU="}
+
+'''headers = {"Accept": "application/yang-data+json",
+           "Content-Type": "application/yang-data+json",
+           "Authorization": "Basic ZGV2ZWxvcGVyOkMxc2NvMTIzNDU="}'''
 
 
-r = requests.get(url, auth=(username, password), headers=headers, verify=False)
+r = requests.get(url, headers=headers, verify=False)
+## REQUEST PUEDE CONTENER EL PARAMETRO AUTH= O BASIC EN EL HEADER
+##r = requests.get(url, auth=(username, password), headers=headers, verify=False)
 
 rJson = r.json()
 

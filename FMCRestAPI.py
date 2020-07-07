07b1e91df8ae1c27a068aa22550dc2d8919136f7
oscar <oscar@oscar>
2020-07-06 21:12:35 -0500

    FMC Script modificado

--- FMCRestAPI.py
+++ FMCRestAPI.py
@@ -1,23 +1,38 @@
 import requests
 import json
 import urllib3
+import sys
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 username = "oscsalga"
-password = "Fht2gvae"
+password = "UGkfrDxb"
 urlToken = "https://fmcrestapisandbox.cisco.com/api/fmc_platform/v1/auth/generatetoken"
 urlDevices = "https://fmcrestapisandbox.cisco.com/api/fmc_config/v1/domain/e276abec-e0f2-11e3-8169-6d9ed49b625f/devicegroups/devicegrouprecords"
+urlObjectsMonitoring = "https://fmcrestapisandbox.cisco.com/api/fmc_config/v1/domain/e276abec-e0f2-11e3-8169-6d9ed49b625f/object/applications"
 headers = {"Content-type": "application/json"}
 
 r = requests.post(urlToken, headers=headers, auth=(username, password), verify=False)
-print(r.headers) # Nos muestra X-auth-access-token dentro del diccionario
-token = r.headers["X-auth-access-token"]
 
+if r.status_code == 204:  # STATUS CODE FOR OK BUT WITH NO CONTENT
+    token = r.headers["X-auth-access-token"]
+else:
+    print("Code 401")  # CODE 401 ERROR ON TOKEN
+    sys.exit()
+print(r.headers)  # Nos muestra X-auth-access-token dentro del diccionario
+
+print(token)
 headers['X-auth-access-token'] = token
 
-r2 = requests.get(urlDevices, headers=headers, verify=False)
+r = requests.get(urlDevices, headers=headers, verify=False)
 
-print(r2.json()["links"])
-for x in r2.json()["items"]:
+print(r.json()["links"])
+for x in r.json()["items"]:
     print(x["name"])
+
+r = requests.get(urlObjectsMonitoring, headers=headers, verify=False)
+#print(r.json()["items"])
+
+for apps in r.json()["items"]:
+    print(f"Apps Monitoring: {apps['name']}")
+
